5481c74e82912d239766829bf3f07c6b88a24b7e
Brian <Brian.reed1202@gmail.com>
2021-10-24 18:54:32 -0400

    added unit test pseudocode

---
+++ src/test/java/baseline/DataTableManagerTest.java
@@ -0,0 +1,14 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataTableManagerTest {
+    @Test
+    void createDataTableTest(){
+        //mimic input to scene
+        //see if new table created
+    }
+
+}
---
+++ src/test/java/baseline/DataTableTest.java
@@ -0,0 +1,22 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataTableTest {
+    @Test
+    void getItemDescriptionTest(){
+        //mimic input to scene
+        //run method
+        //compare method result to static result
+    }
+
+    void getItemDueDateTest(){
+        //same as above
+    }
+
+    void getItemComplete(){
+        //same as above
+    }
+}
