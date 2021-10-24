c036732f92cc87ced44ae411e346b538cb920248
Brian <Brian.reed1202@gmail.com>
2021-10-24 18:48:34 -0400

    added more pseudocode

---
+++ src/main/java/baseline/DataTable.java
@@ -0,0 +1,35 @@
+package baseline;
+
+public class DataTable {
+    void addItem(){
+        //get description
+        //get due date
+        //get complete
+        //add data to data table
+        //return [][]
+    }
+
+    void removeItem(){
+        //remove item from data table
+    }
+
+    String getItemDescription(){
+        //get description from scene
+        //temp save and send to addItem
+    }
+
+    int getItemDueDate(){
+        //get due date from scene
+        //temp save and send to addItem
+    }
+
+    boolean getItemComplete(){
+        //get complete from scene
+        //temp save and send to addItem
+    }
+
+    void saveDataTable(){
+        //copy all info from saved array
+        //write all info to text file formatted
+    }
+}
---
+++ src/main/java/baseline/DataTableManager.java
@@ -0,0 +1,13 @@
+package baseline;
+
+public class DataTableManager {
+    public Object[][] createDataTable(){
+        //new create new instance of dataTable
+        //return table pointer
+        return;
+    }
+
+    public void deleteDataTable(){
+        //remove instance of dataTable
+    }
+}
